2be5aefd0412ed68c78f14b2e58d9dcb2b358772
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-08-28 02:00:54 +0000

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -1,115 +1,152 @@
-# Taken from megadlbot_oss <https://github.com/eyaadh/megadlbot_oss/blob/master/mega/webserver/routes.py>
-# Thanks to Eyaadh <https://github.com/eyaadh>
-# Credit @LazyDeveloper.
-# Please Don't remove credit.
-# Born to make history @LazyDeveloper !
-# Thank you LazyDeveloper for helping us in this Journey
-# 🥰  Thank you for giving me credit @LazyDeveloperr  🥰
-# for any error please contact me -> telegram@LazyDeveloperr or insta @LazyDeveloperr 
-
-
+import re
 import math
-import secrets
 import logging
+import secrets
+import time
 import mimetypes
 from aiohttp import web
-from mega.common import Common
-from mega.telegram import MegaDLBot
-from mega.helpers.seerd_api import SeedrAPI
-from mega.telegram.utils.custom_download import TGCustomYield, chunk_size, offset_fix
+from aiohttp.http_exceptions import BadStatusLine
+from lazybot import multi_clients, work_loads, LazyPrincessBot
+from server.exceptions import FIleNotFound, InvalidHash
+from zzint import StartTime, __version__
+from util.custom_dl import ByteStreamer
+from util.time_format import get_readable_time
+from util.render_template import render_page
+from info import *
+
 
 routes = web.RouteTableDef()
 
-
-@routes.get("/")
-async def root_route_handler(request):
-    bot_details = await MegaDLBot.get_me()
-    return web.json_response({"status": "running",
-                              "server_permission": "Limited" if len(Common().allowed_users) > 0 else "Open",
-                              "bot_associated_w": bot_details.username})
+@routes.get("/", allow_head=True)
+async def root_route_handler(_):
+    return web.Response(text="Success is the journey of millions of failure and lots of hard work & smart logics. - @LazyDeveloperr -/-- Welcome to our #LazyFamily 🥰")
 
 
-@routes.get("/stream/{message_id}")
-async def stream_handler(request):
+@routes.get(r"/watch/{path:\S+}", allow_head=True)
+async def stream_handler(request: web.Request):
     try:
-        message_id = int(request.match_info['message_id'])
-        return await media_streamer(request, message_id)
-    except ValueError as e:
-        logging.error(e)
-        raise web.HTTPNotFound
+        path = request.match_info["path"]
+        match = re.search(r"^([a-zA-Z0-9_-]{6})(\d+)$", path)
+        if match:
+            secure_hash = match.group(1)
+            id = int(match.group(2))
+        else:
+            id = int(re.search(r"(\d+)(?:\/\S+)?", path).group(1))
+            secure_hash = request.rel_url.query.get("hash")
+        return web.Response(text=await render_page(id, secure_hash), content_type='text/html')
+    except InvalidHash as e:
+        raise web.HTTPForbidden(text=e.message)
+    except FIleNotFound as e:
+        raise web.HTTPNotFound(text=e.message)
+    except (AttributeError, BadStatusLine, ConnectionResetError):
+        pass
+    except Exception as e:
+        logging.critical(e.with_traceback(None))
+        raise web.HTTPInternalServerError(text=str(e))
 
+@routes.get(r"/{path:\S+}", allow_head=True)
+async def stream_handler(request: web.Request):
+    try:
+        path = request.match_info["path"]
+        match = re.search(r"^([a-zA-Z0-9_-]{6})(\d+)$", path)
+        if match:
+            secure_hash = match.group(1)
+            id = int(match.group(2))
+        else:
+            id = int(re.search(r"(\d+)(?:\/\S+)?", path).group(1))
+            secure_hash = request.rel_url.query.get("hash")
+        return await media_streamer(request, id, secure_hash)
+    except InvalidHash as e:
+        raise web.HTTPForbidden(text=e.message)
+    except FIleNotFound as e:
+        raise web.HTTPNotFound(text=e.message)
+    except (AttributeError, BadStatusLine, ConnectionResetError):
+        pass
+    except Exception as e:
+        logging.critical(e.with_traceback(None))
+        raise web.HTTPInternalServerError(text=str(e))
 
-async def media_streamer(request, message_id: int):
-    range_header = request.headers.get('Range', 0)
-    media_msg = await MegaDLBot.get_messages(Common().bot_dustbin, message_id)
-    file_properties = await TGCustomYield().generate_file_properties(media_msg)
-    file_size = file_properties.file_size
+class_cache = {}
+
+async def media_streamer(request: web.Request, id: int, secure_hash: str):
+    range_header = request.headers.get("Range", 0)
+    
+    index = min(work_loads, key=work_loads.get)
+    faster_client = multi_clients[index]
+    
+    if MULTI_CLIENT:
+        logging.info(f"Client {index} is now serving {request.remote}")
+
+    if faster_client in class_cache:
+        tg_connect = class_cache[faster_client]
+        logging.debug(f"Using cached ByteStreamer object for client {index}")
+    else:
+        logging.debug(f"Creating new ByteStreamer object for client {index}")
+        tg_connect = ByteStreamer(faster_client)
+        class_cache[faster_client] = tg_connect
+    logging.debug("before calling get_file_properties")
+    file_id = await tg_connect.get_file_properties(id)
+    logging.debug("after calling get_file_properties")
+    
+    if file_id.unique_id[:6] != secure_hash:
+        logging.debug(f"Invalid hash for message with ID {id}")
+        raise InvalidHash
+    
+    file_size = file_id.file_size
 
     if range_header:
-        from_bytes, until_bytes = range_header.replace('bytes=', '').split('-')
+        from_bytes, until_bytes = range_header.replace("bytes=", "").split("-")
         from_bytes = int(from_bytes)
         until_bytes = int(until_bytes) if until_bytes else file_size - 1
     else:
         from_bytes = request.http_range.start or 0
-        until_bytes = request.http_range.stop or file_size - 1
+        until_bytes = (request.http_range.stop or file_size) - 1
 
-    req_length = until_bytes - from_bytes
+    if (until_bytes > file_size) or (from_bytes < 0) or (until_bytes < from_bytes):
+        return web.Response(
+            status=416,
+            body="416: Range not satisfiable",
+            headers={"Content-Range": f"bytes */{file_size}"},
+        )
 
-    new_chunk_size = await chunk_size(req_length)
-    offset = await offset_fix(from_bytes, new_chunk_size)
+    chunk_size = 1024 * 1024
+    until_bytes = min(until_bytes, file_size - 1)
+
+    offset = from_bytes - (from_bytes % chunk_size)
     first_part_cut = from_bytes - offset
-    last_part_cut = (until_bytes % new_chunk_size) + 1
-    part_count = math.ceil(req_length / new_chunk_size)
-    body = TGCustomYield().yield_file(media_msg, offset, first_part_cut, last_part_cut, part_count,
-                                      new_chunk_size)
+    last_part_cut = until_bytes % chunk_size + 1
 
-    file_name = file_properties.file_name if file_properties.file_name \
-        else f"{secrets.token_hex(2)}.jpeg"
-    mime_type = file_properties.mime_type if file_properties.mime_type \
-        else f"{mimetypes.guess_type(file_name)}"
+    req_length = until_bytes - from_bytes + 1
+    part_count = math.ceil(until_bytes / chunk_size) - math.floor(offset / chunk_size)
+    body = tg_connect.yield_file(
+        file_id, index, offset, first_part_cut, last_part_cut, part_count, chunk_size
+    )
 
-    return_resp = web.Response(
+    mime_type = file_id.mime_type
+    file_name = file_id.file_name
+    disposition = "attachment"
+
+    if mime_type:
+        if not file_name:
+            try:
+                file_name = f"{secrets.token_hex(2)}.{mime_type.split('/')[1]}"
+            except (IndexError, AttributeError):
+                file_name = f"{secrets.token_hex(2)}.unknown"
+    else:
+        if file_name:
+            mime_type = mimetypes.guess_type(file_id.file_name)
+        else:
+            mime_type = "application/octet-stream"
+            file_name = f"{secrets.token_hex(2)}.unknown"
+
+    return web.Response(
         status=206 if range_header else 200,
         body=body,
         headers={
-            "Content-Type": mime_type,
+            "Content-Type": f"{mime_type}",
             "Content-Range": f"bytes {from_bytes}-{until_bytes}/{file_size}",
-            "Content-Disposition": f'attachment; filename="{file_name}"',
+            "Content-Length": str(req_length),
+            "Content-Disposition": f'{disposition}; filename="{file_name}"',
             "Accept-Ranges": "bytes",
-        }
+        },
     )
-
-    if return_resp.status == 200:
-        return_resp.headers.add("Content-Length", str(file_size))
-
-    return return_resp
-
-
-@routes.get("/seedr/{user_id}/{folder_id}")
-async def seedr_stream_handler(request):
-    folder_id = request.match_info['folder_id']
-    user_id = int(request.match_info['user_id'])
-    headers = await SeedrAPI().get_seedr_headers(user_id, folder_id)
-
-    size = int(headers['Content-Length'])
-
-    offset = request.http_range.start or 0
-    limit = request.http_range.stop or size
-
-    range_head = {"Range": f"bytes={offset}-{limit}"}
-
-    body = SeedrAPI().yield_seedr_stream(user_id, folder_id, range_head)
-
-    return_resp = web.Response(
-        status=206 if offset else 200,
-        body=body,
-        headers={
-            "Content-Range": f"bytes {offset}-{size}/{size}",
-            "Content-Length": str(limit - offset),
-            'Content-Type': headers.get('Content-Type'),
-            'Content-Disposition': f'inline; filename="{secrets.token_hex(4)}.zip"',
-            'Accept-ranges': 'bytes'
-        }
-    )
-
-    return return_resp
